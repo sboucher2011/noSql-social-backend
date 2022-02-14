afdbc8d2af06741102afd3136d2419dd9947a1e6
sboucher <sboucher2011@gmail.com>
2022-02-14 16:56:44 -0500

    updated user routes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
+app.use(require('./routes/api'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-hub', {
   useNewUrlParser: true,
