4fa125aa69f292eae45287d3be468a92e5e0a8ea
sboucher <sboucher2011@gmail.com>
2022-02-14 16:53:00 -0500

    added user routes

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-hu
   useUnifiedTopology: true
 });
 
-// Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
