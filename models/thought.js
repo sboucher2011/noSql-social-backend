cab8149940ee53f43e29275faed66e0623be7736
sboucher <sboucher2011@gmail.com>
2022-02-14 19:34:38 -0500

    added max length to reaction and added email match check

--- models/thought.js
+++ models/thought.js
@@ -10,6 +10,7 @@ const ReactionSchema = new Schema(
     reactionBody: {
       type: String,
       required: true,
+      maxlength: 280
     },
     username: {
       type: String,
--- models/user.js
+++ models/user.js
@@ -1,6 +1,11 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+var validateEmail = function(email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email)
+};
+
 const UserSchema = new Schema(
     {
       username: {
@@ -13,7 +18,9 @@ const UserSchema = new Schema(
         type: String,
         unique: true,
         required: true,
-        trim: true
+        trim: true,
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
       },
       friends: [],
       thoughts: [
